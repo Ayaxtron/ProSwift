e58a3a08b3c897e6f7044715ff768ee81f4385e8
Ayax <casarrubias2837@gmail.com>
2020-12-09 19:47:37 -0600

    Functions & Monads

---
+++ Chapter6_FunctionalProgramming.playground/Pages/Functions & Monads.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Functor is a container that implements map(), arrays and optionals are functors and monads
+//A monad is a functor that also implements flatMap()
+//In other terms, a functor is a data type that can be mapped over, and a monad is a functor that also can be flatmapped over
+//: [Next](@next)
